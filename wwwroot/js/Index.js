2610c5a6b4c75abb9600c0bbbe2751be7c93e530
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-11-12 21:15:48 -0300

    cards terminadas

--- wwwroot/js/Index.js
+++ wwwroot/js/Index.js
@@ -11,3 +11,11 @@ function redirectToProducts() {
 function redirectToCreateCategory() {
     window.location.href = '/Categorias/CreateCategoria';
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("h1");
+    title.addEventListener("mouseenter", () => {
+        title.classList.add("wiggle");
+        setTimeout(() => title.classList.remove("wiggle"), 1000);
+    });
+});
